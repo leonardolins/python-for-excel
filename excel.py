06316cb08429cc40e54c6809fcd81763bce347e5
leonardolins <leonardolins@yahoo.com>
2025-03-05 14:25:47 -0300

    Add new files and modified files

--- excel.py
+++ excel.py
@@ -1,6 +1,7 @@
 """This module offers a read and write function to get
 2-dimensional lists in and out of Excel files.
 """
+
 import re
 import itertools
 import datetime as dt
@@ -70,9 +71,9 @@ def read(sheet, first_cell="A1", last_cell=None):
                 # Handle the different cell types
                 if sheet.cell(r, c).ctype == xlrd.XL_CELL_DATE:
                     value = xlrd.xldate.xldate_as_datetime(
-                        sheet.cell(r, c).value, sheet.book.datemode)
-                elif sheet.cell(r, c).ctype in [xlrd.XL_CELL_EMPTY,
-                                                xlrd.XL_CELL_BLANK]:
+                        sheet.cell(r, c).value, sheet.book.datemode
+                    )
+                elif sheet.cell(r, c).ctype in [xlrd.XL_CELL_EMPTY, xlrd.XL_CELL_BLANK]:
                     value = None
                 elif sheet.cell(r, c).ctype == xlrd.XL_CELL_ERROR:
                     value = error_text_from_code[sheet.cell(r, c).value]
@@ -86,9 +87,12 @@ def read(sheet, first_cell="A1", last_cell=None):
 
     # OpenPyXL
     elif openpyxl and isinstance(
-            sheet,
-            (openpyxl.worksheet.worksheet.Worksheet,
-             openpyxl.worksheet._read_only.ReadOnlyWorksheet)):
+        sheet,
+        (
+            openpyxl.worksheet.worksheet.Worksheet,
+            openpyxl.worksheet._read_only.ReadOnlyWorksheet,
+        ),
+    ):
         if last_cell is None:
             # used range
             last_cell = (sheet.max_row, sheet.max_column)
@@ -97,17 +101,27 @@ def read(sheet, first_cell="A1", last_cell=None):
         if not isinstance(last_cell, tuple):
             last_cell = openpyxl.utils.cell.coordinate_to_tuple(last_cell)
         data = []
-        for row in sheet.iter_rows(min_row=first_cell[0], min_col=first_cell[1],
-                                   max_row=last_cell[0], max_col=last_cell[1],
-                                   values_only=True):
+        for row in sheet.iter_rows(
+            min_row=first_cell[0],
+            min_col=first_cell[1],
+            max_row=last_cell[0],
+            max_col=last_cell[1],
+            values_only=True,
+        ):
             data.append(list(row))
         return data
 
     # pyxlsb
     elif pyxlsb and isinstance(sheet, pyxlsb.worksheet.Worksheet):
-        errors = {"0x0": "#NULL!", "0x7": "#DIV/0!", "0xf": "#VALUE!",
-                  "0x17": "#REF!", "0x1d": "#NAME?", "0x24": "#NUM!",
-                  "0x2a": "#N/A"}
+        errors = {
+            "0x0": "#NULL!",
+            "0x7": "#DIV/0!",
+            "0xf": "#VALUE!",
+            "0x17": "#REF!",
+            "0x1d": "#NAME?",
+            "0x24": "#NUM!",
+            "0x2a": "#N/A",
+        }
         if not isinstance(first_cell, tuple):
             first_cell = xl_cell_to_rowcol(first_cell)
             first_cell = (first_cell[0] + 1, first_cell[1] + 1)
@@ -116,11 +130,14 @@ def read(sheet, first_cell="A1", last_cell=None):
             last_cell = (last_cell[0] + 1, last_cell[1] + 1)
         data = []
         # sheet.rows() is a generator that requires islice to slice it
-        for row in itertools.islice(sheet.rows(),
-                                    first_cell[0] - 1,
-                                    last_cell[0] if last_cell else None):
-            data.append([errors.get(cell.v, cell.v) for cell in row]
-                        [first_cell[1] - 1 : last_cell[1] if last_cell else None])
+        for row in itertools.islice(
+            sheet.rows(), first_cell[0] - 1, last_cell[0] if last_cell else None
+        ):
+            data.append(
+                [errors.get(cell.v, cell.v) for cell in row][
+                    first_cell[1] - 1 : last_cell[1] if last_cell else None
+                ]
+            )
         return data
     else:
         raise TypeError(f"Couldn't handle sheet of type {type(sheet)}")
@@ -147,16 +164,14 @@ def write(sheet, values, first_cell="A1", date_format=None):
         options={"default_date_format": "mm/dd/yy"}
     """
     # OpenPyXL
-    if openpyxl and isinstance(
-            sheet, openpyxl.worksheet.worksheet.Worksheet):
+    if openpyxl and isinstance(sheet, openpyxl.worksheet.worksheet.Worksheet):
         if date_format is None:
             date_format = "mm/dd/yy"
         if not isinstance(first_cell, tuple):
             first_cell = openpyxl.utils.coordinate_to_tuple(first_cell)
         for i, row in enumerate(values):
             for j, value in enumerate(row):
-                cell = sheet.cell(row=first_cell[0] + i,
-                                  column=first_cell[1] + j)
+                cell = sheet.cell(row=first_cell[0] + i, column=first_cell[1] + j)
                 cell.value = value
                 if date_format and isinstance(value, (dt.datetime, dt.date)):
                     cell.number_format = date_format
@@ -184,11 +199,9 @@ def write(sheet, values, first_cell="A1", date_format=None):
         for i, row in enumerate(values):
             for j, cell in enumerate(row):
                 if isinstance(cell, (dt.datetime, dt.date)):
-                    sheet.write(i + first_cell[0], j + first_cell[1],
-                                cell, date_format)
+                    sheet.write(i + first_cell[0], j + first_cell[1], cell, date_format)
                 else:
-                    sheet.write(i + first_cell[0], j + first_cell[1],
-                                cell)
+                    sheet.write(i + first_cell[0], j + first_cell[1], cell)
     else:
         raise TypeError(f"Couldn't handle sheet of type {type(sheet)}")
 
@@ -243,7 +256,7 @@ def xl_cell_to_rowcol(cell_str):
     expn = 0
     col = 0
     for char in reversed(col_str):
-        col += (ord(char) - ord("A") + 1) * (26 ** expn)
+        col += (ord(char) - ord("A") + 1) * (26**expn)
         expn += 1
 
     # Convert 1-index to zero-index
